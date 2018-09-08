d73003dbb530b8edcfbd01ee0d794c4beb0a496c
Ahmed Nefzaoui <nefzaoui.ahmed@hotmail.com>
2018-09-08 13:59:58 +0100

    Code update

---
+++ example.js
@@ -0,0 +1,7 @@
+// Small example to test the functionality of the lib.
+
+const instalink = require('./index');
+
+instalink.getURL('BnOefCyhYrr').then((r) => {
+  console.log(r);
+});
--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const instalink = require('./instalink');
+'use strict';
 
-instalink.getURL('BnOefCyhYrr').then((r) => {
-  console.log(r);
-});
+const puppeteer = require('puppeteer');
+
+/**
+
+Instagram scraping is tricky due to the nature of modern-day JS frameworks.
+The server doesn't return rendered HTML anymore. Instead it passes content,
+plus the rules of rendering it to the browser and leaves it for the browser.
+Because of that simply doing a smiple request doesn't return the desured result.
+
+So we have to actually render the page first using a browser then pull out the info
+needed from it.
+
+One great way to render web pages using a controlled APIs set it Puppeteer.
+
+
+CSS Selector
+img.FFVAD
+
+U-A: Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.3 (KHTML, like Gecko) Version/8.0 Mobile/12A4345d Safari/600.1.4
+
+Instagram Post URL example: https://www.instagram.com/p/BnN7whpBHwC/
+
+**/
+
+async function getImageURL(postID) {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  let postURL = `https://www.instagram.com/p/${postID}/`;
+  await page.goto(postURL);
+
+  const imgURL = await page.evaluate(() => {
+    let imgElement = document.querySelector('img.FFVAD');
+    if (imgElement !== null) {
+      return {"status": "OK", "Message": document.querySelector('img.FFVAD').src};
+    } else {
+      return {"status": "ERROR", "Message": "Cannot find the image requested. Are you sure the post ID is correct?"}
+    }
+  });
+
+  await browser.close();
+  return imgURL;
+};
+
+module.exports.getURL = getImageURL;
